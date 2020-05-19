eb3506371bef35a486a6a3f6360d7fec735ceb4d
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2020-05-18 20:02:26 -0400

    React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [isPressed, setIsPressed] = useState(false)
+
+  useEffect(() => {
+    generateCanvas()
+  }, [])
+
+  const generateCanvas = () => {
+    console.log("Loaded the canvas JS")
+    const can = document.getElementById('canvas1');
+    const ctx = can.getContext('2d');
+    ctx.lineWidth = 8;
+
+    const can2 = document.getElementById('canvas2')
+    const ctx2 = can2.getContext('2d');
+    ctx2.lineWidth = 1;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas id="canvas1" width="224" height="224" style={{ border: "1px solid black"}} />
+      <canvas id="canvas2" width="28" height="28" style={{ border: "1px solid black"}} />
     </div>
   );
 }
